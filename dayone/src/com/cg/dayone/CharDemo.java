772a4c0fdeea487919c1bd75c227c55c3f696dfe
lenovo legion 5 <lenovo legion 5@LAPTOP-FU74ML41>
2023-04-18 15:03:10 +0530

    first commit

---
+++ dayone/src/com/cg/dayone/CharDemo.java
@@ -0,0 +1,20 @@
+package com.cg.dayone;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		//size of char is 2 bytes
+		char letter = 'a';
+		System.out.println("The value of letter is " +letter);
+		
+		char digit = '8';
+		System.out.println("The value of digit is " +digit);
+		
+		char symbol = '$';
+		System.out.println("The value of symbol is " +symbol);
+		
+
+	}
+
+}
